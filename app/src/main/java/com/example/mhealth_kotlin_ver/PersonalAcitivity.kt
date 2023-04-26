fb18c7d1f9b93d3f6d9852e6ab59d713954f1828
jiseungmin <sm46384638@naver.com>
2023-04-26 16:24:28 +0900

    layout기초

--- app/src/main/java/com/example/mhealth_kotlin_ver/PersonalAcitivity.kt
+++ app/src/main/java/com/example/mhealth_kotlin_ver/PersonalAcitivity.kt
@@ -4,20 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mhealth_kotlin_ver.databinding.ActivityPersonalBinding
-import com.example.mhealth_kotlin_ver.databinding.ActivitySurveyBinding
 
-class SurveyActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySurveyBinding
+class PersonalAcitivity: AppCompatActivity() {
+    private lateinit var binding : ActivityPersonalBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySurveyBinding.inflate(layoutInflater)
+        binding = ActivityPersonalBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.NextButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, SurveyActivity::class.java)
             startActivity(intent)
         }
-
 
     }
 }
--- app/src/main/java/com/example/mhealth_kotlin_ver/SplashActivity.kt
+++ app/src/main/java/com/example/mhealth_kotlin_ver/SplashActivity.kt
@@ -34,7 +34,7 @@ class SplashActivity  : AppCompatActivity() {
         }
 
         Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, PersonalAcitivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
             finish()
